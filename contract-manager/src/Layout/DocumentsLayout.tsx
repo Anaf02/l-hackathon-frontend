23de1fb51757e68a343a75e8c8205ce9030fbfd0
Anaf02 <ana.florescu.02@gmail.com>
2024-05-12 02:37:52 +0300

    login fix

--- contract-manager/src/Layout/DocumentsLayout.tsx
+++ contract-manager/src/Layout/DocumentsLayout.tsx
@@ -8,29 +8,26 @@
 // - On document list item click => display document in viewer and change right bar details. => implement using a useState Hook where on click you set current document to the one clicked
 // - Right bar receives current document as prop, document viewer receives document as a prop.
 
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import ContractsSidebar from "../components/ContractsSidebar";
 import { PdfDocument } from "../components/PdfDocumentModel";
 import { useEffect, useState } from "react";
 import PdfTextViewer from "../components/PdfTextViewer";
 import PdfEditableTextViewer from "../components/PdfEditableTextViewer";
+import { useSimpleAuth } from "../components/Context/AuthContext/useSimpleAuthHook";
 
 interface Props {
   pdfDocuments: PdfDocument[];
   isLoading: boolean;
-  // userRole: string;
+  userRole: string;
 }
 
-function DocumentsLayout({ pdfDocuments, isLoading }: Props) {
+function DocumentsLayout({ pdfDocuments, isLoading, userRole }: Props) {
   const [documents, setDocuments] = useState(pdfDocuments);
-  const [role, setRole] = useState<number>(1);
+  const { logout } = useSimpleAuth();
 
   useEffect(() => setDocuments(pdfDocuments), [pdfDocuments]);
   const [currentDocument, setCurrentDocument] = useState<PdfDocument>();
-
-  const handleRoleChange = (event: any) => {
-    setRole(event.target.value);
-  };
 
   return (
     <>
@@ -44,18 +41,10 @@ function DocumentsLayout({ pdfDocuments, isLoading }: Props) {
             />
           </Col>
           <Col md="8" className="vh-100">
-            <Row>
-              <Col className="mt-4 md-7 p-3">
-                <Form.Select onChange={handleRoleChange}>
-                  <option value="1">Finance Role</option>
-                  <option value="2">Legal Role</option>
-                </Form.Select>
-              </Col>
-            </Row>
             <Row className="h-75">
               <Col className="mt-4 md-7 h-100">
                 <PdfEditableTextViewer
-                  readOnly={role == 2}
+                  readOnly={userRole == "legal"}
                   text={currentDocument?.text}
                 />
               </Col>
@@ -63,6 +52,14 @@ function DocumentsLayout({ pdfDocuments, isLoading }: Props) {
           </Col>
           <Col md="2">
             {/* TODO: right sideBar */}
+            <Button
+              className="ml-auto"
+              onClick={(e) => {
+                logout();
+              }}
+            >
+              Logout
+            </Button>
             <h1>Important information</h1>
           </Col>
         </Row>
